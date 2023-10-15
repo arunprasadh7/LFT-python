6a3ac6cc6aadd744d27a0070e857aeba0d029a40
arunprasadh7 <m.arunprasadh96@gmail.com>
2023-10-15 11:32:45 +0530

    Super function

--- OOPS/oopdemo.py
+++ OOPS/oopdemo.py
@@ -26,50 +26,67 @@ from user import *  # imports entire user class
 # print(u1.users)
 
 
-# inheritance
-# student class
-s1 = Student()
-s1.greet_student()
-s1.register()
-s1.login()
+# # inheritance
+# # student class
+# s1 = Student()
+# s1.greet_student()
+# s1.register()
+# s1.login()
 
-# faculty class
-f1 = Faculty()
-f1.greet_faculty()
-f1.register()
-f1.login()
+# # faculty class
+# f1 = Faculty()
+# f1.greet_faculty()
+# f1.register()
+# f1.login()
 
-# multilevel inheritance
-# temp faculty class
-t1 = Tempfaculty()
-t1.greetTempFaculty()
-t1.greet_faculty()
-t1.register()
-t1.login()
+# # multilevel inheritance
+# # temp faculty class
+# t1 = Tempfaculty()
+# t1.greetTempFaculty()
+# t1.greet_faculty()
+# t1.register()
+# t1.login()
 
-# multiple inheritance
-# studentfaculty class that inherits both student and faculty class
-print('\nMultiple Inheritance:')
-sf1 = StudentFaculty()
-sf1.register()
-sf1.login()
-sf1.greet_student()
-sf1.greet_faculty()
+# # multiple inheritance
+# # studentfaculty class that inherits both student and faculty class
+# print('\nMultiple Inheritance:')
+# sf1 = StudentFaculty()
+# sf1.register()
+# sf1.login()
+# sf1.greet_student()
+# sf1.greet_faculty()
 
-#method overiding
-#creating a method greet in user class and overiding it in student subclass
-print('\nMethod overiding')
-u1 = User()
-u1.greet()
+# #method overiding
+# #creating a method greet in user class and overiding it in student subclass
+# print('\nMethod overiding')
+# u1 = User()
+# u1.greet()
 
-s1.greet()
+# s1.greet()
 
-#method chaining - joining multiple method in single statement
-#the prior function must return self inorder to chain or else error will be thrown
-print('\nMethod Chaining:')
-u2 = User()
-u2.register()
-u2.login()
-#the above code can be simplified as
-print('Chained code:')
-u2.login().register() #calling two methods in single statement(method chaining).login returns self
+# #method chaining - joining multiple method in single statement
+# #the prior function must return self inorder to chain or else error will be thrown
+# print('\nMethod Chaining:')
+# u2 = User()
+# u2.register()
+# u2.login()
+# #the above code can be simplified as
+# print('Chained code:')
+# u2.login().register() #calling two methods in single statement(method chaining).login returns self
+
+
+#Super function
+print('\nSuper function:')
+sup1 = Student('Prasadh','99433','python', '30k')
+print(sup1.username)
+print(sup1.password)
+print(sup1.course)
+print(sup1.fee)
+sup1.greet_student()
+
+sup2 = Student('Storm', 'ttyl','Java','50k')
+print(sup2.username)
+print(sup2.password)
+print(sup2.course)
+print(sup2.fee)
+sup2.greet_student()
--- OOPS/user.py
+++ OOPS/user.py
@@ -2,12 +2,12 @@
 
 class User():
 
-    # users = 0	#class variable
+    users = 0	#class variable
 
-    # def __init__(self, username, password):
-    # 	self.username = username
-    # 	self.password = password	#instance variables/instance attributes
-    # 	User.users += 1
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password  # instance variables/instance attributes
+        User.users += 1
 
     def register(self):
         print(f'Registering .')
@@ -17,20 +17,26 @@ class User():
         return self
 
     def greet(self):
-     	print('Hello user.This is a user class method.')
+        print('Hello user.This is a user class method.')
 
 
 # Inheritance
 
 # creating a student subclass
 class Student(User):  # Student inherits User
+
+    def __init__(self, username, password, course, fee):
+        super().__init__(username,password)
+        self.course = course
+        self.fee = fee
+
     def greet_student(self):
-        print('Hi student...')
+        print('Hello student.' + self.username)
 
     def greet(self):
-    	print('Hello student. This is a student class method.')
+        print('Hello student. THis is a student class method.')
 
-# creaiing another subclass Faculty
+# creating another subclass Faculty
 
 
 class Faculty(User):
